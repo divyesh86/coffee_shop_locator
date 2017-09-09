93cbb86a68b242dce1241a8bb176eab1114388f5
divyeshmotiwalla <divyesh86@gmail.com>
2017-09-08 22:47:36 -0700

    first commit

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 let path = require('path');
 module.exports = {
     filePath:  path.resolve(__dirname,"../source/resources/locations.csv"),
-    testFilePath: path.resolve(__dirname, "../test/resources/commodity.txt"),
+    testFilePath: path.resolve(__dirname, "../test/resources/locations.csv"),
     apiHead: 'https://maps.googleapis.com/maps/api/geocode/json?address=',
     apiTail: '&key=',
     geocodeKey: 'AIzaSyBV1oZNTWcBudTaj7IzFGwPFQzgDF0MnTg',
--- test/services/geolocation/geolocationServiceTest.js
+++ test/services/geolocation/geolocationServiceTest.js
@@ -7,32 +7,34 @@ let findNearestService = require('../../../source/services/geoLocation/findNeare
 let storage = require('../../../source/cache/storage');
 
 
-// describe('insert records', function() {
-//     it('from a file', async function() {
-//         storage.storage.clearSync();
-//         let result = fileReaderService.writeToCache(config.testFilePath);
-//         assert.equal('', result);
-//     });
-// });
-//
-//
-// describe('fetch lat long', function() {
-//     it('from an address ', async function() {
-//         let testAddress = "986 Market St, San Francisco, CA, USA";
-//         let result = await addressService.fetchLatLong(testAddress);
-//         assert.equal('', result);
-//     });
-// });
-//
-//
-//
-// describe('find nearest coffee shop', function() {
-//     it('from an address ', async function() {
-//         let testAddress = "988 Market St, San Francisco, CA, USA";
-//         let result = await findNearestService.findNearest(testAddress);
-//         assert.equal('', result);
-//     });
-// });
+describe('insert records', function() {
+    it('from a file', async function() {
+        storage.storage.clearSync();
+        let result = fileReaderService.writeToCache(config.testFilePath);
+        assert.equal('Successfully saved 3 records', result.message);
+    });
+});
+
+
+describe('fetch lat long', function() {
+    it('from an address ', async function() {
+        let testAddress = "986 Market St, San Francisco, CA, USA";
+        let result = await addressService.fetchLatLong(testAddress);
+        let expected = '{"lat":37.7826339,"lng":-122.4102865}';
+        assert.equal(expected, JSON.stringify(result));
+    });
+});
+
+
+
+describe('find nearest coffee shop', function() {
+    it('from an address ', async function() {
+        let testAddress = "988 Market St, San Francisco, CA, USA";
+        let result = await findNearestService.findNearest(testAddress);
+        let expected = '{"id":"1","name":" Equator Coffees & Teas","address":" 986 Market St","latitude":" 37.782394430549445","longitude":" -122.40997343121123"}';
+        assert.equal(expected, JSON.stringify(result));
+    });
+});
 
 
 
