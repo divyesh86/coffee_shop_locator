5d66fdef1c660c4b3cbc7c0b801991b815353462
divyeshmotiwalla <divyesh86@gmail.com>
2017-09-08 22:35:39 -0700

    first commit

--- test/services/geolocation/geolocationServiceTest.js
+++ test/services/geolocation/geolocationServiceTest.js
@@ -7,32 +7,32 @@ let findNearestService = require('../../../source/services/geoLocation/findNeare
 let storage = require('../../../source/cache/storage');
 
 
-describe('insert records', function() {
-    it('from a file', async function() {
-        storage.storage.clearSync();
-        let result = fileReaderService.writeToCache(config.testFilePath);
-        assert.equal('', result);
-    });
-});
-
-
-describe('fetch lat long', function() {
-    it('from an address ', async function() {
-        let testAddress = "986 Market St, San Francisco, CA, USA";
-        let result = await addressService.fetchLatLong(testAddress);
-        assert.equal('', result);
-    });
-});
-
-
-
-describe('find nearest coffee shop', function() {
-    it('from an address ', async function() {
-        let testAddress = "988 Market St, San Francisco, CA, USA";
-        let result = await findNearestService.findNearest(testAddress);
-        assert.equal('', result);
-    });
-});
+// describe('insert records', function() {
+//     it('from a file', async function() {
+//         storage.storage.clearSync();
+//         let result = fileReaderService.writeToCache(config.testFilePath);
+//         assert.equal('', result);
+//     });
+// });
+//
+//
+// describe('fetch lat long', function() {
+//     it('from an address ', async function() {
+//         let testAddress = "986 Market St, San Francisco, CA, USA";
+//         let result = await addressService.fetchLatLong(testAddress);
+//         assert.equal('', result);
+//     });
+// });
+//
+//
+//
+// describe('find nearest coffee shop', function() {
+//     it('from an address ', async function() {
+//         let testAddress = "988 Market St, San Francisco, CA, USA";
+//         let result = await findNearestService.findNearest(testAddress);
+//         assert.equal('', result);
+//     });
+// });
 
 
 
